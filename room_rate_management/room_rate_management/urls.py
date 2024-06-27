db91183a67329bd35779c71bdb62a2697010aacb
sawar27769 <sawar.singh@zs.com>
2024-06-27 11:18:48 +0530

    Added Homepage

--- room_rate_management/room_rate_management/urls.py
+++ room_rate_management/room_rate_management/urls.py
@@ -4,11 +4,13 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from .views import index
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Room Rate Management API",
       default_version='v1',
-      description="API documentation for the Room Rate Management  project",
+      description="API documentation for the Room Rate Management project",
 
    ),
    public=True,
@@ -16,6 +18,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', index, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('room_rates.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
